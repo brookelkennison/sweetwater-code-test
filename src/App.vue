c61a095a97d3c01b40f237caa05ae32092dcb135
Brooke Kennison <brookelkennison@gmail.com>
2019-10-23 21:22:00 -0500

    rebuilt

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
     <div id="app">
         <Header />
-        <Items />
-        <Footer />
     </div>
 </template>
 
 <script>
-    import Header from "@/components/Header";
-    import Items from "@/components/Items";
-    import Footer from "@/components/Footer";
+    import Header from "./components/Header";
 
     export default {
         name: "App",
         components: {
-            Header,
-            Items,
-            Footer
+            Header
         }
     };
 </script>
