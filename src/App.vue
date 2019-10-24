09c120661b5b26f88e0e13dd9277adb3a7fd7a17
Brooke Kennison <brookelkennison@gmail.com>
2019-10-23 22:16:27 -0500

    rendered item array

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
     <div id="app">
         <Header />
+        <Items />
+        <ul>
+            <li>{{myItems}}</li>
+        </ul>
     </div>
 </template>
 
 <script>
     import Header from "./components/Header";
+    import Items from "./components/Items";
+    import items from "../public/items.json";
 
     export default {
         name: "App",
+        data(){
+            return{
+                myItems: items
+            }
+        },
         components: {
-            Header
+            Header,
+            Items
         }
-    };
+    }
+
 </script>
