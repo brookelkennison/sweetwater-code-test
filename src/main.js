93098d565b9f875ffbfbdd03c46b4ca69ac8f53b
Brooke Kennison <brookelkennison@gmail.com>
2019-11-06 21:30:08 -0500

    updated products.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App.vue';
 import '../public/items.json';
+
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
---
+++ src/store/modules/products.js
@@ -0,0 +1,13 @@
+import Products from '../public/items.json';
+
+// state
+    const state = {
+
+    };
+// getters
+
+// actions
+
+// mutations
+
+// export
